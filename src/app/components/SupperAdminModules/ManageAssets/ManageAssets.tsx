82c368cab41011c4790691fb755e4bb8e551075e
rokunuzzamanbhuiya <rokunuzzamanbhuiya@gmail.com>
2025-09-08 15:24:45 +0600

    some update

--- src/app/components/SupperAdminModules/ManageAssets/ManageAssets.tsx
+++ src/app/components/SupperAdminModules/ManageAssets/ManageAssets.tsx
@@ -149,14 +149,10 @@ export default function ManageAssets() {
                   <td className="px-6 py-4 whitespace-nowrap text-gray-900 border border-[#D0D5DD]">{asset.model}</td>
                   <td className="px-6 py-4 whitespace-nowrap border border-[#D0D5DD]">
                     <button className="text-indigo-600 hover:text-indigo-900 mr-4 border border-gray-300 rounded-md pt-3 pb-3 pl-3 pr-3 hover:bg-gray-50 text-gray-700 cursor-pointer">
-                      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                      </svg>
+                      <img src="/pencil.svg" alt="4ami" />
                     </button>
                     <button className="text-red-600 hover:text-red-900 border border-gray-300 rounded-md pt-3 pb-3 pl-3 pr-3 hover:bg-gray-50 text-gray-700 cursor-pointer">
-                      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                      </svg>
+                      <img src="/bin.svg" alt="4ami" />
                     </button>
                   </td>
                 </tr>
