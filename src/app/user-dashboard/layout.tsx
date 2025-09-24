9685281561601d6714b92d65c4048100131a70be
Raihan <aboraihan14@gmail.com>
2025-09-24 13:00:04 +0600

    log out toast added

--- src/app/user-dashboard/layout.tsx
+++ src/app/user-dashboard/layout.tsx
@@ -33,7 +33,18 @@ export default function UserDashboardLayout({
   const handleLogout = () => {
     localStorage.removeItem('authToken');
     localStorage.removeItem('userData');
-    toast.success('Logged out successfully');
+    toast.success('Logged out successfully',{
+      position: 'top-center',
+      icon: null,
+      style: {
+        background: 'black',
+        color: 'white',
+        borderRadius: '4px',
+        
+      }
+    } 
+      
+    )
     router.push('/login');
   };
 
