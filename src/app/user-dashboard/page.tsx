42a191bc0f54913cf6bef074609b4b65d4cd968a
Raihan <aboraihan14@gmail.com>
2025-09-30 10:01:22 +0600

    UserPrjoectDropdown added

--- src/app/user-dashboard/page.tsx
+++ src/app/user-dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
+import UserProjectDropdown from "./UserProjectDropdown";
 
 export default function UserDashboardPage() {
   const statsData = [
@@ -22,22 +23,7 @@ export default function UserDashboardPage() {
         </div>
       </div>
       <div className="flex justify-end mb-5">
-        <button className="bg-[#ED272C] text-white px-8 py-2 cursor-pointer rounded-lg hover:bg-red-600 flex items-center space-x-2">
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
-          <span className="text-white">Create New Project</span>
-        </button>
+           <UserProjectDropdown/>
       </div>
       {/* Stats Cards */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-3">
